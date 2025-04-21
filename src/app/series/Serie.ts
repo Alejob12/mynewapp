0e40de7cf2ef24505d37b70471f232e5c48844f3
Alejandro Bernal López <m.lizarazob@uniandes.edu.co>
2025-04-21 00:25:56 -0500

    Ultimo Commit

--- src/app/series/Serie.ts
+++ src/app/series/Serie.ts
@@ -1,14 +1,24 @@
 export class Serie {
- name: string;
- channel: string;
- seasons: number;
+  id: number;
+  name: string;
+  channel: string;
+  seasons: number;
+  description: string;
+  webpage: string;
+  poster: string;
 
- public constructor(name: string, channel: string, seasons: number) {
-   this.name = name;
-   this.channel = channel;
-   this.seasons = seasons;
+
+ public constructor(id: number, name: string, channel: string, seasons: number, description: string, webpage: string, poster: string) {
+  this.id = id;
+  this.name = name;
+  this.channel = channel;
+  this.seasons = seasons;
+  this.description = description;
+  this.webpage = webpage;
+  this.poster = poster;
  }
-}
+ }
+
 
 
 
